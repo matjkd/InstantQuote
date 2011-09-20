96dc8b319a861f0950ef56abc3116a49f1cbc7c2
Mat Sadler <mat@redstudio.co.uk>
2011-09-20 12:06:57 +0000

    added html5 boilerplate

---
+++ application/controllers/quote.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Quote extends CI_Controller {
+
+	/**
+	 *
+	 */
+	public function index()
+	{
+		$this->load->view('template/ker');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
