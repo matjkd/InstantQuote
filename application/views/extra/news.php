791292f5c0b86fed8d8b4e44dc60e0859314f3f8
Mat Sadler <mat@redstudio.co.uk>
2012-09-13 15:07:17 +0000

    added extra folder

---
+++ application/views/extra/news.php
@@ -0,0 +1,27 @@
+   <h1>Latest News</h1>
+   <div class="newsfeed">
+           <?php $newsfeed = file_get_contents('http://www.ker.co.uk/residential-news.feed?type=rss');
+            $xml = simplexml_load_string($newsfeed); 
+           
+           foreach($xml->channel->item as $row):
+           	$descriptionTrim = str_replace("/<p>\s+<\/p>/","","$row->description");
+			
+			
+			$string = strstr($row->author, '(', false);
+			$authorTrim = str_replace(')', '', $string);
+			$authorTrim = str_replace('(', '', $authorTrim);
+			$datestring = "%l %j%S %M  %Y %G:%i:%s";
+			
+			$unix = human_to_unix($row -> pubDate);
+			$timeTrim = mdate($datestring, $unix);
+
+           ?>
+			 <div class="feedItem" style="clear:both;"> 
+			   <h2><a href="<?=$row -> link ?>"><?=$row -> title ?></a></h2>
+			   <?=$timeTrim ?> | <strong>by <?=$authorTrim ?></strong>
+			     <div style="clear:both; padding-top:10px;"><?=$descriptionTrim ?></div>
+			   <div style="clear:both; padding-top:10px;"><a href="<?=$row -> link ?>">Read More...</a></div>
+			   
+			 </div>   
+		   <?php endforeach; ?>
+           </div>
