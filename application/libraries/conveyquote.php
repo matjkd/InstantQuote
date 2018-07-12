f0f05a51e479acba150805ea62fa4717dedef253
mat sadler <github@redstudio.co.uk>
2018-07-12 13:23:55 +0100

    Testing removing discount

--- application/libraries/conveyquote.php
+++ application/libraries/conveyquote.php
@@ -167,7 +167,7 @@ else {
             /* TOTAL PURCHASE */
             $data['feevat'] = ($data['purchase_fee'] + $data['banktransfer_purchase'] + $data['mortgagefee'] + $data['leaseholdfee'] + $data['stamp_duty_forms']) * $vat;
             $data['feevat'] = number_format($data['feevat'], 2, '.', '');
-            $data['totalpurchase'] = $data['purchase_fee'] + $data['leaseholdfee'] + $data['feevat'] + $data['land_fee'] + $data['newStamp'] + $data['banktransfer_purchase'] + $data['stamp_duty_forms'] + $data['mortgagefee'] + $data['landcharge'] + $data['localsearch'] + $data['priority_search'] - $data['firsttimediscount'];
+            $data['totalpurchase'] = $data['purchase_fee'] + $data['leaseholdfee'] + $data['feevat'] + $data['land_fee'] + $data['newStamp'] + $data['banktransfer_purchase'] + $data['stamp_duty_forms'] + $data['mortgagefee'] + $data['landcharge'] + $data['localsearch'] + $data['priority_search'];
             $data['totalpurchase'] = number_format($data['totalpurchase'], 2, '.', '');
 
             $purchasecount = 1;
